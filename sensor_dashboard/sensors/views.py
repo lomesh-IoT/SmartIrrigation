d89322f650bbff0b4fb5fa081b9724ec173eef83
Lomesh <lomeshmahajan488@gmail.com>
2024-11-17 13:56:40 +0530

    new commit

--- sensor_dashboard/sensors/views.py
+++ sensor_dashboard/sensors/views.py
@@ -5,6 +5,10 @@ from .models import DHTData, PumpData, SoilMoistureData, MotionData
 from django.utils import timezone
 from datetime import timedelta
 from django.views.decorators.csrf import csrf_exempt
+from .serializers import MotionDataSerializer
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
 
 # Variable to simulate pump state
 pump_state = False  # False means off, True means on
@@ -16,6 +20,8 @@ def get_latest_sensor_data(request):
     dht_data = DHTData.objects.filter(timestamp__gte=one_hour_ago).order_by('timestamp')
     soil_data = SoilMoistureData.objects.filter(timestamp__gte=one_hour_ago).order_by('timestamp')
     motion_data = MotionData.objects.filter(timestamp__gte=one_hour_ago).order_by('timestamp')
+    pump_data = PumpData.objects.last()
+    pump_status = pump_data.pumpStatus if pump_data else 'OFF'
     
     # Structure data for Chart.js
     response_data = {
@@ -23,12 +29,42 @@ def get_latest_sensor_data(request):
         'humidity': [data.humidity for data in dht_data],
         'soil_moisture': [data.moisture_level for data in soil_data],
         'motion': [1 if data.motion_detected else 0 for data in motion_data],
-        'timestamps': [data.timestamp.strftime("%H:%M:%S") for data in dht_data]
+        'timestamps': [data.timestamp.strftime("%H:%M:%S") for data in dht_data],
+        'pump_status': pump_status
     }
     return JsonResponse(response_data)
 
+def get_data_for_dashboard(request):
+    dht_data = DHTData.objects.last()
+    temp = dht_data.temperature
+    humid = dht_data.humidity
+    soilData = SoilMoistureData.objects.last()
+    soil_data = soilData.moisture_level
+    latest_motion_data = MotionData.objects.last()
+    serializer = MotionDataSerializer(latest_motion_data)
+    print("Serializer =>",serializer)
+    motionData = serializer.data
+    print("motionData =>", motionData)
+    motion_data = motionData["motion_detected"]
+    print("motion_data =>", motion_data)
+    pump_data = PumpData.objects.last()
+    pump_status = pump_data.pumpStatus if pump_data else 'OFF'
+    
+    # Structure data for Chart.js
+    response_data = {
+        'temperature': temp,
+        'humidity': humid,
+        'soil_moisture': soil_data,
+        'motion': motion_data,
+        'pump_status': pump_status
+    }
+    return JsonResponse( response_data)
+
+def chart(request):
+    return render(request, 'sensors/charts.html')
+
 def dashboard(request):
-    return render(request, 'sensors/sensor_dashboard.html')
+    return render(request, 'sensors/dashboard.html')
 
 def get_pump_status(request):
     pump_data = PumpData.objects.last()
